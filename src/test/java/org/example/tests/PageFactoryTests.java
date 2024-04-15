5828e6337ee5ca6ec465ead203a3c892fddb8378
akoba2 <a.skoba89@gmail.com>
2024-04-15 15:07:01 +0300

    added allure annotations

--- src/test/java/org/example/tests/PageFactoryTests.java
+++ src/test/java/org/example/tests/PageFactoryTests.java
@@ -1,5 +1,6 @@
 package org.example.tests;
 
+import io.qameta.allure.*;
 import org.example.pages.TextBoxPage;
 import org.example.utils.PropertyHandler;
 import org.openqa.selenium.WebDriver;
@@ -8,6 +9,7 @@ import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import static io.qameta.allure.SeverityLevel.BLOCKER;
 import static org.example.driverfactory.DriverFactory.getDriver;
 import static org.testng.Assert.assertEquals;
 
@@ -34,12 +36,23 @@ public class PageFactoryTests {
         driver.get("https://demoqa.com/text-box");
     }
 
+    @Description("Selenium positive test for Jenkins")
+    @Epic("First Epic")
+    @Story("First tory")
+    @Feature("First Feature")
+    @TmsLinks(value = {@TmsLink("TC-321"), @TmsLink("TC-322")})
     @Test
     public void positiveTest() {
         System.out.println("getCheckBoxText method result: " + textBoxPage.getCheckBoxText());
         assertEquals(textBoxPage.getCheckBoxText(), "Check Box", "Was wrong text");
     }
 
+    @Description("Selenium negative test for Jenkins")
+    @Epic("Second Epic")
+    @Story("Second tory")
+    @Feature("Second Feature")
+    @Issue("BUG-1234")
+    @Severity(BLOCKER)
     @Test
     public void negativeTest() {
         System.out.println("getCheckBoxText method result: " + textBoxPage.getCheckBoxText());
